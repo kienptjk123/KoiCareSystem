7f8312c8d64b970a720b880bc12f102607b75cd7
locleabcd <locleabcd@gmail.com>
2024-10-01 16:33:31 +0700

    adding Growth History function

--- src/main/java/com/swpproject/koi_care_system/models/GrowthHistory.java
+++ src/main/java/com/swpproject/koi_care_system/models/GrowthHistory.java
@@ -1,24 +1,27 @@
 package com.swpproject.koi_care_system.models;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.sql.Date;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class GrowHistory {
+public class GrowthHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Date createDate;
+    private LocalDate createDate;
     private String physique;
     private Double length;
     private Double weight;
-
+    private String imageUrl;
     @ManyToOne
     @JoinColumn(name="koifish_id")
     private KoiFish koiFish;
